5f689f5038b321efc292de92e30634fe26bb31c3
Sergio Panjoj <sergiopanjoj@gmail.com>
2019-11-14 00:56:31 -0800

    feed UI

---
+++ codepath-instagram/Views/FeedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedTableViewCell.swift
+//  codepath-instagram
+//
+//  Created by Sergio P. on 11/14/19.
+//  Copyright © 2019 Sergio P. All rights reserved.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
